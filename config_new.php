17e5cbf96e588d22feb078a453d7382a1de77504
dredei <xkadidas@gmail.com>
2014-02-22 17:02:25 +0200

    Добавлены коменты в конфиге

--- config_new.php
+++ config_new.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = array();
-$config[ 'ya_login' ] = '';
-$config[ 'ya_password' ] = '';
-$config[ 'ya_backups_dir' ] = '/WABackups/';
+$config[ 'ya_login' ] = ''; // логин yandex
+$config[ 'ya_password' ] = ''; // пароль yandex
+$config[ 'ya_backups_dir' ] = '/WABackups/'; // название папки, куда будут сохраняться бекапы
 
